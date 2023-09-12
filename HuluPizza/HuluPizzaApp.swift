fe2da0c84f98dd7e7ed2bc7b772932e07b095d7c
Haden Olfers <haden.w.olfers@exxonmobil.com>
2023-09-12 14:05:16 -0500

    Initial Commit

---
+++ HuluPizza/HuluPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuluPizzaApp.swift
+//  HuluPizza
+//
+//  Created by Haden Olfers on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HuluPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
